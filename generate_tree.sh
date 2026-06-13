9ba72e58c43d4789c1364a9cacefab2440e19646
AdrienG <a.gras30730@gmail.com>
2026-06-13 20:24:48 +0200

    [chore]<project>: Project Tree + Project Tree Gen

--- generate_tree.sh
+++ generate_tree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tree -I ".vscode|build|spdlog|uni_algo|.git" > projectTree.txt
+tree -I ".vscode|build|spdlog|uni_algo|.git|input_excel|save_html|test_save_html" > projectTree.txt
 
 echo "Project tree generated in projectTree.txt"
