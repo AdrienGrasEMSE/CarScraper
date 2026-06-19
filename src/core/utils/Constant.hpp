d906beeab213c921cf3da0e13052c7dd544ac26f
AdrienG <a.gras30730@gmail.com>
2026-06-19 14:33:43 +0200

    [fix]<Constant>: fixed some tab

--- src/core/utils/Constant.hpp
+++ src/core/utils/Constant.hpp
@@ -43,8 +43,8 @@ namespace CarScraper {
     constexpr       double      DEFAULT_DOUBLE = 9999.0;        ///< Sentinel default value for double
     constexpr       double      ERROR_DOUBLE   = -1.0;          ///< Sentinel error value for double
 
-    inline const    std::string DEFAULT_STR    = "#";          ///< Sentinel default value for string
-    inline const    std::string ERROR_STR      = "error";      ///< Sentinel error value for string
+    inline const    std::string DEFAULT_STR    = "#";           ///< Sentinel default value for string
+    inline const    std::string ERROR_STR      = "error";       ///< Sentinel error value for string
 
 
 
