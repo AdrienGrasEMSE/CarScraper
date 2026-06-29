7e16ea1675e9514e52d21b77b53f6f8849a03873
AdrienG <a.gras30730@gmail.com>
2026-06-29 11:49:13 +0200

    [improvement]<ArgusScraper>: inherit GenericScrap

--- src/core/scraper/ArgusScraper.hpp
+++ src/core/scraper/ArgusScraper.hpp
@@ -13,11 +13,7 @@
 
 
 // Imports
-#include "core/models/Entity.hpp"
-#include "io/httpClient/HttpClient.hpp"
-#include "io/htmlSaver/HtmlSaver.hpp"
-#include <string>
-#include <vector>
+#include "core/scraper/GenericCarScraper.hpp"
 
 
 /**
@@ -30,40 +26,7 @@ namespace CarScraper {
      * 
      * @brief Scrap the Argus website.
      */
-    class ArgusScraper : public Entity {
-
-        // =========================================================================
-        // Private data and methods
-        // =========================================================================
-        private:
-    
-            // Scraping related data
-            std::string                 _outputFolder;  ///< Path to the ouput folder which will contains all saved files
-            std::string                 _carBrand;      ///< The car targeted car brand
-            std::string                 _carModel;      ///< The car targeted car model
-
-
-            // Internal object
-            CarScraper::HttpClient      _client;        ///< HTTP Client
-            CarScraper::HtmlSaver       _saver;         ///< HTML Saver
-
-
-
-
-
-            // -------------------------------------------------------------------------
-            // Internal Helpers
-            // -------------------------------------------------------------------------
-
-            /**
-             * @brief Shuffle a vector
-             */
-            template<typename T>
-            void _shuffle(std::vector<T>& vector);
-
-
-
-
+    class ArgusScraper : public GenericCarScraper {
 
         // =========================================================================
         // Public data and methods
@@ -80,67 +43,6 @@ namespace CarScraper {
             ArgusScraper();
 
 
-            /**
-             * @brief Default Destructor.
-             */
-            ~ArgusScraper();
-
-
-
-
-            // -------------------------------------------------------------------------
-            // Getters
-            // -------------------------------------------------------------------------
-
-            /** @brief Gets the output folder path
-             *  @return The output folder path
-             */
-            const std::string& getOutputFolder() const { return _outputFolder; }
-
-
-            /** @brief Gets the car brand
-             *  @return The car brand
-             */
-            const std::string& getCarBrand() const { return _carBrand; }
-
-
-            /** @brief Gets the car model
-             *  @return The car model
-             */
-            const std::string& getCarModel() const { return _carModel; }
-
-
-
-
-
-            // -------------------------------------------------------------------------
-            // Setters
-            // -------------------------------------------------------------------------
-
-
-            /**
-             * @brief Sets the output folder path
-             * @param outputFolder The output folder path
-             * @note Checks if the directory exists
-             */
-            void setOutputFolder(const std::string& outputFolder);
-
-
-            /**
-             * @brief Set the car brand
-             * @param carBrand The car brand
-             */
-            void setCarBrand(const std::string& carBrand);
-
-
-            /**
-             * @brief Set the car model
-             * @param carModel The car model
-             */
-            void setCarModel(const std::string& carModel);
-
-
-
 
 
             // -------------------------------------------------------------------------
@@ -152,6 +54,7 @@ namespace CarScraper {
              * @param startDate The start date which the scraping start after
              * @param endDate The end date which the scraping start before
              * @return ERROR_CODE / SUCCESS_CODE wheter the method is succesful or not
+             * @note The accepted date format is "AAAA"
              */
             int scrapModel(const std::string& startDateStr, const std::string& endDateStr);
 
